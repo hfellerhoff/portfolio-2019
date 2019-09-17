666d8af24efda3a939a1207fe71b55e799d730ca
Henry Fellerhoff <henryfellerhoff@gmail.com>
2019-09-17 13:20:58 -0500

    Updated font-family

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -4,3 +4,8 @@ export const JoinMe = () => {
     const image = require('../images/joinme.png');
     return <img src={image} className="project-item image" alt="Project Image" />
 };
+
+export const Arrow = () => {
+    const image = require('../images/arrow.png');
+    return <img src={image} className='arrow-image' />
+};
