ffe1a1c26219e83bb2199ff0ab50f44d4b6940c3
Henry Fellerhoff <henryfellerhoff@gmail.com>
2019-11-29 11:11:54 -0500

    Moved Open IPA from repository into its own repository

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Portfolio from './Portfolio';
-import IPATranslator from './apps/IPATranslator/App';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Portfolio} />
-      <Route path='/ipa' component={IPATranslator} />
     </BrowserRouter>
   );
 };
